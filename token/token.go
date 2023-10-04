8b723dbb8192a78dd0e9c3d17e3f4114e437f646
Tushar Dahiya <tusharxoxoxo@gmail.com>
2023-10-04 15:22:43 +0530

    creating tokens for our cute little goblin shark

--- token/token.go
+++ token/token.go
@@ -1,1 +1,14 @@
-package token 
+//token/token.go 
+
+//in our token package we are going to define our token struct and token type
+
+package token
+
+type TokenType string
+
+type Token struct{
+    Type TokenType
+    Literal string
+}
+
+
